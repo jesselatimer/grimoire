d7ad1f62e511a3393e88f8debef2eb93c65b711f
Jesse Latimer <jesse.latimer@gmail.com>
2015-08-11 10:03:24 -0700

    add shelves to wireframes

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  image_url       :string
+#  canon           :string
+#  bio             :text
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  before_validation :ensure_session_token
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  
+end
---
+++ db/migrate/20150811163704_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, index: true, unique: true
+      t.string :email, null: false, index: true, unique: true
+      t.string :image_url
+      t.string :canon
+      t.text   :bio
+
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
